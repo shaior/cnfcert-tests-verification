ebf41dbb22f9734c546bcd7a17f99b9d1ec45a1e
sobarzan <sobarzan@redhat.com>
2022-03-01 09:37:54 +0200

    commented imports on pod-recreation

--- tests/lifecycle/tests/lifecycle_pod_recreation.go
+++ tests/lifecycle/tests/lifecycle_pod_recreation.go
@@ -1,9 +1,9 @@
 package tests
 
-import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
-)
+// import (
+// 	. "github.com/onsi/ginkgo"
+// 	. "github.com/onsi/gomega"
+// )
 
 // var _ = Describe("lifecycle lifecycle-pod-recreation", func() {
 
